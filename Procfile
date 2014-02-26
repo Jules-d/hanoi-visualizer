36a6707d71b6d79381278356cbc996358d3e5dc9
Jules <julian.debhal@gmail.com>
2014-02-26 18:49:18 +1000

    lein new heroku hanoi-visualizer

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein with-profile production trampoline run -m hanoi-visualizer.web
---
+++ test/hanoi_visualizer/web_test.clj
@@ -0,0 +1,6 @@
+(ns hanoi-visualizer.web-test
+  (:require [clojure.test :refer :all]
+            [hanoi-visualizer.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
