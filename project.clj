7771357adde44e70c540e7cbe2b10a93aacfeda8
Jules <julian.debhal@gmail.com>
2014-03-06 07:31:53 +1000

    Add dependencies (deployed)

--- project.clj
+++ project.clj
@@ -7,10 +7,13 @@
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [ring/ring-devel "1.1.0"]
+                 [ring/ring-core "1.2.1"]
                  [ring-basic-authentication "1.0.1"]
                  [environ "0.2.1"]
-                 [com.cemerick/drawbridge "0.0.6"]]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [hiccup "1.0.5"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[environ/environ.lein "0.2.1"]
+            [lein-ring "0.8.10"]]
   :hooks [environ.leiningen.hooks]
   :profiles {:production {:env {:production true}}})
