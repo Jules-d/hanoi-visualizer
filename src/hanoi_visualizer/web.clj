7efbc67b24babfeb1e8dd8e717623fab216266fc
Jules <julian.debhal@gmail.com>
2014-03-06 20:06:49 +1000

    restore http prefix to generated urls

--- src/hanoi_visualizer/web.clj
+++ src/hanoi_visualizer/web.clj
@@ -65,9 +65,9 @@
                        (catch Exception e [:div "Exception: " (.getMessage e)]))
         example2  "[(\"a\",\"c\"), (\"a\",\"b\"), (\"c\",\"b\")]"
         example3 "[(\"a\",\"b\"),(\"a\",\"c\"),(\"b\",\"c\"),(\"a\",\"b\"),(\"c\",\"a\"),(\"c\",\"b\"),(\"a\",\"b\")]"
-        port (:server-port req)
+        req-port (:server-port req)
         server (:server-name req)
-        url (str server (if port ":" port) port)]
+        url (str "http://" server (if req-port ":" req-port) req-port "/")]
     {:status 200
      :headers {"Content-Type" "text/html"}
      :body  (hp/html5 [:head]
